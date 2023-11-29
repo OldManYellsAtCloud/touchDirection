2d158d9fc80bbfca3290d620995ed911e6ba66a5
skandigraun <skandigraun@gmail.com>
2023-11-29 18:30:27 +0100

    find touchscreen dyamically instead of hardcoding it

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,94 @@
 #include <cstdio>
 #include <thread>
+#include <format>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "logger.h"
 
 #include <sdbus-c++/sdbus-c++.h>
 
 #include "touchhandler.h"
 
+#define INPUT_EVENT_PATH "/dev/input/{}"
+#define INPUT_DEVICES "/proc/bus/input/devices"
+#define PHYS_TYPE "P: Phys"
+#define TOUCHSCREEN_TYPE "input/ts"
+#define HANDLER_TYPE "H: Handlers"
+#define EVENT_HANDLER "event"
 
 using namespace std;
 
+bool isLineTouchscreenName(const std::string& line){
+    return line.starts_with(PHYS_TYPE) && line.ends_with(TOUCHSCREEN_TYPE);
+}
+
+bool isLineHandlerType(const std::string& line){
+    return line.starts_with(HANDLER_TYPE);
+}
+
+bool isEventId(const std::string& line){
+    return line.starts_with(EVENT_HANDLER);
+}
+
+std::string extractEventId(const std::string& line){
+    std::stringstream tokens{line};
+    std::string eventId;
+
+    do {
+        tokens >> eventId;
+    } while (!isEventId(eventId) && tokens.good());
+
+    if (!isEventId(eventId))
+        return "";
+
+    return eventId;
+}
+
+std::string getNextEventId(std::ifstream& stream){
+    std::string line;
+    std::string eventId = "";
+    while (stream.good()){
+        std::getline(stream, line);
+        if (isLineHandlerType(line)){
+            eventId = extractEventId(line);
+            break;
+        }
+    }
+    return eventId;
+}
+
+std::string findTouchScreenEvent(){
+    std::ifstream inputDevices{INPUT_DEVICES, ios_base::in};
+    std::string evendId;
+    if (!inputDevices.is_open()){
+        LOG(LOG_ERR, "Could not open input devices!");
+        exit(1);
+    }
+
+    std::string line;
+    std::string eventId;
+    while (inputDevices.good()){
+        std::getline(inputDevices, line);
+        if (isLineTouchscreenName(line)){
+            eventId = getNextEventId(inputDevices);
+            break;
+        }
+    }
+
+    inputDevices.close();
+    return eventId;
+}
+
 int main()
 {
+
+    std::string touchScreenEvent = findTouchScreenEvent();
+    if (touchScreenEvent == "") {
+        LOG(LOG_ERR, "Could not find touchscreen device!");
+        exit(1);
+    }
+
     const char* serviceName = "org.gspine.gesture";
     auto connection = sdbus::createSystemBusConnection(serviceName);
 
@@ -21,7 +99,7 @@ int main()
     sdbusObject->registerSignal(interfaceName, "touchEvent", "s");
     sdbusObject->finishRegistration();
 
-    TouchHandler th {"/dev/input/event3", sdbusObject.get(), interfaceName};
+    TouchHandler th {std::format(INPUT_EVENT_PATH, touchScreenEvent), sdbusObject.get(), interfaceName};
     std::thread t {&TouchHandler::run, th};
 
     connection->enterEventLoop();
